0d2f459c595ed3d4b00f0d8248823c571b95b0d9
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2025-12-02 12:29:09 +0530

    Add files via upload

---
+++ Classwork/program91.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Digit
+{
+    public int SumDigits(int iNo)
+    {
+        int iDigit=0;
+        iDigit=1;
+        int iSum=0;
+
+        while(iNo!=0)
+        {
+            iDigit=iNo%10;
+            iSum+=iDigit;
+            iNo/=10;
+        }
+        return iSum;
+    }
+}
+
+class program91
+{
+    public static void main(String A[])
+    {
+        int iValue=0;
+        int iRet=0;
+
+        Scanner sobj=new Scanner(System.in);
+        System.out.println("Enter number :");
+        iValue=sobj.nextInt();
+
+        Digit dobj=new Digit();
+        iRet=dobj.SumDigits(iValue);
+
+        System.out.println(iRet);
+        //important
+        
+        sobj=null;
+        dobj=null;
+
+        System.gc();
+    }
+}
